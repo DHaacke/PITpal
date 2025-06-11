7d5fe7b39efc2b308f96e244a69f1e2d7bb00b84
Doug Haacke <dhaacke@gmail.com>
2025-06-11 17:09:53 -0600

    Initial Commit

---
+++ PITPal/PITPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PITPalApp.swift
+//  PITPal
+//
+//  Created by Doug Haacke on 6/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PITPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
